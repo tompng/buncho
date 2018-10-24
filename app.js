f7ef1bbeb32c94ab251a028f248a3f0d62c0d4f0
tompng <tomoyapenguin@gmail.com>
2018-10-24 21:03:30 +0900

    drill, beak-open

--- app.js
+++ app.js
@@ -47,12 +47,42 @@ class BunchoShape {
       ],
     }
   }
+  drillShape() {
+    return {
+      eye: { x: 0.6, y: 0.1 },
+      up: [{ x: 0.8, y: 0.2 },{ x: 0.6, y: 0.28 }, { x: 0.35, y: 0.3 },{ x: 0, y: 0.35 }, { x: -0.4, y: 0.3 }, { x: -0.9, y: 0.2}],
+      down: [{x:-0.9, y:0.2},{ x: -0.4, y: -0.3 }, { x: 0.1, y: -0.4 },{ x: 0.5, y: -0.25 }, {x:0.8, y:0.0 }],
+      beak: [
+        { x: 1, y: 0.08 },
+        { x: 0.8, y: 0.2 },
+        { x: 0.77, y: 0.1 },
+        { x: 0.8, y: 0.0 },
+        { x: 1, y: 0.08 },
+      ],
+      beakOpen: [
+        [
+          { x: 1.02, y: 0.14 },
+          { x: 0.98, y: 0.18 },
+          { x: 0.8, y: 0.2 },
+          { x: 0.77, y: 0.08 },
+          { x: 0.85, y: 0.11 },
+          { x: 1.02, y: 0.14 },
+        ],
+        [
+          { x: 1, y: 0.0 },
+          { x: 0.8, y: 0.08 },
+          { x: 0.8, y: 0.0 },
+          { x: 1, y: 0.0 },
+        ]
+      ]
+    }
+  }
   render(ctx, option) {
     ctx.lineWidth = 0.02
     ctx.lineCap = 'round'
     ctx.lineJoin = 'round'
     ctx.save()
-    const shape = this.normalShape()
+    const shape = option.drill ? this.drillShape() : this.normalShape()
     const legStart = { x: 0, y: -0.3 }
     const leg1 = { x: 0, y: -0.3 }
     const legEndBase = { x: 0.1, y: -0.7 }
@@ -83,25 +113,57 @@ class BunchoShape {
     ctx.strokeStyle = 'black'
     ctx.fill()
     ctx.stroke()
-
+    if (option.drill && option.drill.rotate) {
+      ctx.globalAlpha = 0.5
+      for (let i = 0; i < 2; i++) {
+        const t = 0.1 * (2 * (option.drill.rotate + i) / 2 - 1)
+        ctx.beginPath()
+        ctx.arc(shape.eye.x + 4 * t * t, shape.eye.y + t, 0.08, 0, 2 * Math.PI)
+        ctx.fillStyle = 'black'
+        ctx.strokeStyle = '#f88'
+        ctx.fill()
+        ctx.stroke()
+        ctx.beginPath()
+        ctx.fillStyle = 'white'
+        ctx.fill()
+      }
+      ctx.globalAlpha = 1
+    } else {
+      ctx.beginPath()
+      ctx.arc(shape.eye.x, shape.eye.y, 0.08, 0, 2 * Math.PI)
+      ctx.fillStyle = 'black'
+      ctx.strokeStyle = '#f88'
+      ctx.fill()
+      ctx.stroke()
+      ctx.beginPath()
+      ctx.arc(shape.eye.x + 0.02, shape.eye.y + 0.02, 0.02, 0, 2 * Math.PI)
+      ctx.fillStyle = 'white'
+      ctx.fill()
+    }
     ctx.beginPath()
-    ctx.arc(shape.eye.x, shape.eye.y, 0.08, 0, 2 * Math.PI)
-    ctx.fillStyle = 'black'
-    ctx.strokeStyle = '#f88'
-    ctx.fill()
-    ctx.stroke()
-    ctx.beginPath()
-    ctx.arc(shape.eye.x + 0.02, shape.eye.y + 0.02, 0.02, 0, 2 * Math.PI)
-    ctx.fillStyle = 'white'
-    ctx.fill()
-
-    ctx.beginPath()
-    ctx.curve(shape.beak)
+    if (shape.beakOpen && option.drill && option.drill.open){
+      shape.beakOpen.forEach(b => ctx.curve(b))
+    } else if (option.drill && option.drill.rotate){
+      const rnd = 0.05 * option.drill.rotate
+      ctx.curve(shape.beak.map((p, i) => {
+        return { x: p.x, y: p.y + (i === 0 || i === shape.beak.length - 1 ? rnd : 0) }
+      }))
+    } else {
+      ctx.curve(shape.beak)
+    }
     ctx.closePath()
     ctx.fillStyle = '#f88'
     ctx.strokeStyle = 'black'
     ctx.fill()
     ctx.stroke()
+    if (option.drill && option.drill.rotate) {
+      const rnd = 0.05 * option.drill.rotate
+      ctx.beginPath()
+      ctx.curve([{ x: 0.85, y: 0.18 + rnd }, { x: 0.82, y: 0.15 + rnd }, { x: 0.82, y: 0.03 + rnd }, { x: 0.85, y: -0.02 + rnd }])
+      ctx.curve([{ x: 0.95, y: 0.14 + rnd }, { x: 0.92, y: 0.14 + rnd }, { x: 0.92, y: 0.02 + rnd }, { x: 0.95, y: 0.01 + rnd }])
+      ctx.strokeStyle = 'black'
+      ctx.stroke()
+    }
     ctx.restore()
     if (!option.wings) return
     option.wings.forEach(w => {
